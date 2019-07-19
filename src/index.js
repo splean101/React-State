3fb9e07e8d3e42a96a14b6a5b27441125a5e9cfd
splean101 <101splean@gmail.com>
2019-07-19 10:49:40 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class AnimationText extends React.Component {
     constructor(props) {
         super(props);
         this.state = { text: this.props.text, fontSize: 10 };
-        setInterval(() => this.setState({ fontSize: Math.random() * 10 + 10 }), 1000);
+        setInterval(() => this.setState({ fontSize: Math.ceil(Math.random() * 10 + 10) }), 1000);
     }
     render() {
         return <h1 style={this.state}>{this.props.text}</h1>
